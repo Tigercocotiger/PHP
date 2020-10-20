eb26f95700a5a1c03b694637ce7557173bf9aa51
Tiger <cocogolf9@gmail.com>
2020-10-20 14:34:43 +0200

    oui

--- PHP/test/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*remplacer les valeurs suivantes par les valeurs
- exactes*/
-define ('SERVEUR', "mon_nom_de_serveur");
-define ('LOGIN', "mon_login");
-define ('MDP', "mon_mot_de_passe");
-define ('BDD', "mon_nom_de_bdd");
-?>
